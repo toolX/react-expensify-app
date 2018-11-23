0b6b1fb7c11a40379b51548957e1f8e46d937eb9
Philip Shamsiev <ph.shamsiev@mail.ru>
2018-11-23 15:40:33 +0300

    Add setup for redux devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -8,7 +8,8 @@ export default () => {
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // redux devtools set up 
   );
 
   return store;
